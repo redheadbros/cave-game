16687428431d2217577f7b38b3c7ccf7db2f86a3
Luke Rostad <rostad@ualberta.ca>
2020-01-26 14:22:03 -0700

    added an 'Attractor' class for the special function I use all over the place.

---
+++ core/src/lukesgames/cavegame/Attractor.java
@@ -0,0 +1,23 @@
+package lukesgames.cavegame;
+
+/**
+ * Attractor: a function for 'attracting' a variable to a certain value over time.
+ */
+public class Attractor {
+    private float maxAttractionRate;
+    private float marginOfAttraction;
+
+    public Attractor(float maxRate, float dampingRadius) {
+        maxAttractionRate = maxRate;
+        marginOfAttraction = dampingRadius;
+        assert (marginOfAttraction != 0) : "margin must not be zero";
+    }
+
+    public float getRate(float currentValue, float desiredValue) {
+        if (currentValue >= desiredValue) {
+            return Math.max(-maxAttractionRate, -(currentValue - desiredValue) * maxAttractionRate / marginOfAttraction);
+        } else {
+            return Math.min(maxAttractionRate, -(currentValue - desiredValue) * maxAttractionRate / marginOfAttraction);
+        }
+    }
+}
