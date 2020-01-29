863e0c20513d3d7ca68791c7379d19c851773c1f
Luke Rostad <rostad@ualberta.ca>
2020-01-28 21:53:51 -0700

    deleted old code from Car, starting from scratch. Wheel now turns.

--- core/src/lukesgames/cavegame/Car.java
+++ core/src/lukesgames/cavegame/Car.java
@@ -10,50 +10,17 @@ import static java.lang.Math.abs;
 public class Car {
 
     private CarPhysicsBody physicsBody;
-
-    //testing, turny wheel bits
-    private float desiredWheelRotation;
-
-    //constant values
-    private float frictionCoefficient;
-    private float bodyTurnSpeed; //in physics body
-    private float turnVectorCoefficient;
-
-    private float desiredSpeed; //desired for gas attractor
-
     private Vector2 controlVector;
 
+    private float desiredWheelRotation;
 
     public Car() {
         physicsBody = new CarPhysicsBody();
-
         controlVector = new Vector2(0,0);
+
         physicsBody.position = new Vector2(256,256);
 
-        //test constants for da wheel
         desiredWheelRotation = 60;
-
-        //all of the following are constants
-        frictionCoefficient = 0.3f;
-        bodyTurnSpeed = 180; //degrees per second
-        turnVectorCoefficient = 3f;
-        //note: the turning looks realistic, but is a facade. If the turnVectorCoefficient
-        //  is increased, the car constantly looks like it's 'drifting.' It needs to be
-        //  a formula based off of other stuff, though I'm not sure what.
-        //  also, the car can turn in place like a tank, currently. Not so realistic.
-        //  solution, perhaps: how the BODY of the car turns should be based off of other
-        //  stuff, like speed and the turn vector.
-
-        //ANOTHER STRUCTURAL NOTE:
-        //  Attractor class: just has the one function
-        //  active variables class: contains position, rotation, velocity, turn speed, wheel angle etc
-        //        current name: CarPhysicsBody
-        //  constants class: contains static classes that extend attractor, and all the constants.
-        //    class could for instance be used in different versions of the same vehicle but with
-        //    different parameters
-        //        current name: CarConstants
-
-        desiredSpeed = 300;
 
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
@@ -87,45 +54,15 @@ public class Car {
     }
 
     public void update() {
-
-        float deltaTime = Gdx.graphics.getDeltaTime(); //in seconds
-
-        //turn da wheeeeeel
-        float wheelTurnSpeed = CarConstants.WheelTurnAttractor.getRate(physicsBody.wheelAngle,
-                -desiredWheelRotation * controlVector.x);
-
-        //turn the car (should be moved later)
-        physicsBody.bodyRotation -= deltaTime * bodyTurnSpeed * controlVector.x;
-
-        //calculate forces / acceleration:
-        //calculate friction acceleration
-        Vector2 friction = physicsBody.velocity.cpy().scl(-frictionCoefficient);
-
-        //calculate turning vector, perpendicular to velocity
-        Vector2 turnPower = physicsBody.velocity.cpy().rotate(-90).scl(controlVector.x * turnVectorCoefficient);
-
-        //get acceleration power
+        //initial values
+        float wheelTurningRate = 0;
+        float bodyTurningAcceleration = 0;
         Vector2 acceleration = new Vector2(0,0);
-        float accelerationMagnitude;
-        switch ((int) controlVector.y) {
-            case 1:
-                accelerationMagnitude = CarConstants.AccelerationAttractor.getRate(physicsBody.velocity.len(), desiredSpeed);
-                acceleration.y = accelerationMagnitude;
-                acceleration.rotate(physicsBody.bodyRotation);
-                break;
-            case -1:
-                accelerationMagnitude = CarConstants.BrakeAttractor.getRate(physicsBody.velocity.len(), 0);
-                acceleration = physicsBody.velocity.cpy().nor().scl(accelerationMagnitude);
-                break;
-            default:
-                break;
-        }
 
-        //create acceleration vector
-        acceleration.add(friction);
-        acceleration.add(turnPower);
+        //turning the steering wheel
+        wheelTurningRate = CarConstants.WheelTurnAttractor.getRate(physicsBody.wheelAngle, desiredWheelRotation * -controlVector.x);
 
-        physicsBody.update(acceleration,0, wheelTurnSpeed);
+        physicsBody.update(acceleration,bodyTurningAcceleration,wheelTurningRate);
     }
 
     public CarPhysicsBody getPhysicsBody() {
